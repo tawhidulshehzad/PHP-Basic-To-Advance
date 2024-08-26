0f9f548e218db0fc580806662279a80d1ed91614
shehzad1212 <tawhidulislam261@gmail.com>
2024-08-26 10:18:42 +0600

    array copy

--- arrayCopy.php
+++ arrayCopy.php
@@ -1,2 +1,19 @@
-<?php 
+<?php
 
+$person = array( 'first_name' => 'hello', 'last_name' => 'world' );
+$newPerson = $person;
+// $newPerson = &$person; it's not a new array, just a reference and the first name won't be changed
+$newPerson['first_name'] = 'John';
+
+print_r( $person );
+print_r( $newPerson );
+
+echo("---------------- \n");
+
+function printPerson( $person ) {
+    $person['first_name'] = 'changed';
+    print_r( $person );
+};
+
+printPerson( $person );
+print_r( $person );
