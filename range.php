812a3803de02b1513147187d82ca87b57b8863b2
shehzad1212 <tawhidulislam261@gmail.com>
2024-09-23 04:01:34 +0600

    shuffle, range, random

---
+++ range.php
@@ -0,0 +1,23 @@
+<?php
+
+$numberRange = range( 10, 20 );
+// $numberRange = range( 10, 30, 2 );
+
+// print_r( $numberRange );
+
+$numbers = array();
+
+for ( $i = 0; $i < 10; $i++ ) {
+    array_push( $numbers, $i );
+}
+// print_r( $numbers );
+
+foreach ( range( 10, 20, 2 ) as $evenNumber ) {
+    // echo "evenNumber: " . $evenNumber . "\n";
+
+}
+
+// pick a random number
+$random = mt_rand( 0, 32 );
+shuffle( $numberRange );
+print_r( $numberRange );
