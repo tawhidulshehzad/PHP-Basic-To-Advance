40427b2ec2872379c067004814391fc45b3327ae
shehzad1212 <tawhidulislam261@gmail.com>
2024-09-30 03:10:32 +0600

    string access

--- srting.php
+++ srting.php
@@ -1,7 +1,7 @@
 <?php
 $name = "Rakib";
 
-$string01 = "My name is $name";
+// $string01 = "My name is $name";
 
 // echo $string01;
 
@@ -10,6 +10,18 @@ data1
 data2 {$name}
 data3
 EOD;
-echo $heredoc;
+// echo $heredoc;
+// echo "\n";
+// echo ord( 'A' );
+
 echo "\n";
-echo ord('A');
+
+$string = "Hello, world!";
+// echo $string[0];
+// echo substr( $string, 1 , 3);
+
+$length = strlen( $string );
+// echo $length;
+// echo substr( $string, $length -3);
+
+echo substr( $string, -3);
