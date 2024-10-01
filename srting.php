96c93b5acddf205a636ed251ef1ba79c2347c892
shehzad1212 <tawhidulislam261@gmail.com>
2024-10-01 19:37:51 +0600

    more about string

--- srting.php
+++ srting.php
@@ -25,14 +25,44 @@ $length = strlen( $string );
 // echo substr( $string, $length -3);
 
 // echo substr( $string, -3);
+$newString = array();
+for ( $i = 0; $i < $length; $i++ ) {
+    if ( $string[$i] == " " || $string[$i] == "," ) {
+        continue;
+    } else {
+        array_push( $newString, $string[$i] );
+    }
 
-for ( $i = $length - 1; $i >= 0; $i-- ) {
-    // echo $string[$i];
 }
+
+// print_r( $newString );
+PHP_EOL;
+$joinArry = join( $newString );
+// echo $joinArry;
+
 // echo strrev($string);
 
-$parts = explode(" ", $string);
+$parts = explode( " ", $string );
 // print_r($parts);
 
-$original = join(" ", $parts);
-echo $original;
+$original = join( " ", $parts );
+// echo $original;
+
+$parts01 = str_split( $string );
+// print_r($parts01);
+
+$parts2 = preg_split( '//', $string, -1, PREG_SPLIT_NO_EMPTY );
+// print_r( $parts2 );
+
+$str = 'str ing';
+$chars = preg_split( '//', $str, -1, PREG_SPLIT_NO_EMPTY );
+// print_r($chars);
+
+$str = "ami valo";
+
+// foreach ( count_chars( $str, 1 ) as $strr => $value ) {
+//     echo chr( $strr ) . " occurred  $value times in the string. \n";
+// }
+
+$str = "Hello World!";
+echo count_chars($str,mode: 3);
