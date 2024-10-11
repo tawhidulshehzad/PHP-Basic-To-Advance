ea68ee73435c8898fac8cb65b6082fb2f2ebc853
shehzad1212 <tawhidulislam261@gmail.com>
2024-10-11 06:30:29 +0600

    str lower and upper

---
+++ OOP/firstOop.php
@@ -0,0 +1,38 @@
+<?php
+
+$str1 = 'hello world. i aM fine';
+// $str2 = 'bangladesh';
+$str1 = strtolower( $str1 );
+
+// $strLeanth = strlen( $str1 );
+
+// echo strrev( $str1 );
+
+// echo str_shuffle( $str1 );
+// echo ucfirst( $str1 );
+// echo "\n";
+// echo ucwords( $str1 );
+
+$str2 = explode( ".", $str1 );
+// print_r( $str2 );
+$fixed = "";
+// $finalStr = " ";
+
+// foreach ( $str2 as $item ) {
+
+//     $fixed = ucfirst( trim( $item ) );
+
+//     // echo "\n";
+//     // print_r( $fixed );
+// }
+// print_r( $fixed );
+// $finalStr = join( " .", $fixed );
+// echo $finalStr;
+
+for ( $i = 0; $i < count( $str2 ); $i++ ) {
+    // print_r( $str2[$i] );
+    $str2[$i] = ucfirst( trim( $str2[$i] ) );
+
+}
+$fixed = join( ". ", $str2 );
+echo $fixed;
---
+++ OOP/tempCodeRunnerFile.php
@@ -0,0 +1,6 @@
+<?php
+for ( $i = 0; $i < count( $str2 ); $i++ ) {
+    // print_r( $str2[$i] );
+    $str2[$i] = ucfirst( trim( $str2[$i] ) );
+
+}
