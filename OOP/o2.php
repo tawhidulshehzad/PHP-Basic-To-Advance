1e4a6eb9dcb93481738af67076450c6e8cf7b6d8
shehzad1212 <tawhidulislam261@gmail.com>
2024-10-14 07:50:00 +0600

    more oop

--- OOP/o2.php
+++ OOP/o2.php
@@ -3,15 +3,35 @@
 class Human {
 
     public $name;
+
+    function __construct( $name ) {
+        // echo "new object is created";
+        $this->name = $name;
+    }
     function sayHI() {
-        echo "Hello\n";
-        // $this->sayName();
+        // echo "Hello\n";
+        $this->sayName();
     }
     function sayName() {
-        echo "My name is {$this->name} \n";
+        // echo "My name is {$this->name} \n";
     }
 }
 
-$h1 = new Human();
-$h1->name = "Rubel";
-$h1->sayName();
+$h1 = new Human( "Rubel" );
+$h1->sayHI();
+
+class Car {
+    // public $color = "red";
+    public static $color = "red";
+    static function drive() {
+        // function drive() {
+        echo " Hello how are you";
+    }
+}
+
+$carObj = new Car();
+echo $carObj->color;
+$carObj->drive();
+
+echo Car::$color;
+Car::drive();
---
+++ OOP/o3.php
@@ -0,0 +1,12 @@
+<?php
+
+class Car {
+
+    // constructor auto execution
+    // constructor Cannot return
+    function __construct( $a ) {
+        echo "I am constructor {$a}";
+    }
+
+}
+new Car( 10 );
